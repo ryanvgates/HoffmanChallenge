f3e7e64d00c95d7234723fb58716f08a5eeada40
Ryan Gates <Ryan_Gates@ultimatesoftware.com>
2017-02-14 16:18:56 -0500

    Saving progress

---
+++ HuffmanChallenge/src/FileCreator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FileCreator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfGuids;
+            int.TryParse(args.FirstOrDefault() ?? "1", out numberOfGuids);
+            using (StreamWriter file = new StreamWriter(File.Create("file.txt")))
+            {
+                for (var i = 0; i < numberOfGuids; i++)
+                {
+                    file.WriteLine("{0}", Guid.NewGuid());
+                }
+            }
+        }
+    }
+}
